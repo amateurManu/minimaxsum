aa184ea6449fe91f34cc03b154137fdaca9c612c
Emmanuel Odero <oderoemmanuel6@gmail.com>
2023-02-17 22:58:00 +0300

    push

---
+++ minMaxSum.js
@@ -0,0 +1,19 @@
+function miniMaxSum(arr) {
+    // Write your code here
+    let max = arr[0];
+    let min = arr[0];
+    let maxSum = 0;
+    let minSum = 0;
+    let sum = 0;
+    for (let i=0; i<arr.length; i++){
+        if (arr[i] > max){
+            max = arr[i];
+        }else if (arr[i] < min){
+            min = arr[i];
+        }
+        sum += arr[i];
+        maxSum = sum - min;
+        minSum = sum - max;
+    }
+    return minSum + " " + maxSum;
+}
