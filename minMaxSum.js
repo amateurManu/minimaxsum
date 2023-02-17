7c8a12c8373a4f981356195c84d2fcb50342876d
GeekyHacks <geekyhacks22@gmail.com>
2023-02-18 00:04:46 +0300

    push

--- minMaxSum.js
+++ minMaxSum.js
@@ -21,21 +21,3 @@ function miniMaxSum(arr) {
     return console.log(minSum + " " + maxSum);
 }
 
-
-function miniMaxSum(arr) {
-    // Write your code here
-    let max = arr[0];
-    let min = arr[0];
-    let maxSum = 0;
-    let minSum = 0;
-    let sum = 0;
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i]>max) {
-            max=arr[i];
-            maxSum = arr.pop(max);
-            maxSum++;
-            console.log(maxSum);
-        }
-        
-    }
-}
